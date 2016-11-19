cc20c6b8cb6ccca2ed4ccaddfc35f4ccda4abf5f
Mark Griffith <mgriffit@ualberta.ca>
2016-11-19 15:09:53 -0700

    createSchemas complete

--- computations.py
+++ computations.py
@@ -30,20 +30,42 @@ def partitionMinCover(minCover):
     param: list of tuples of sets
     i.e. minCover = [({},{}),({},{}), ... ,({},{})]
 
-    return: set of lists of tuples of sets
-    i.e. { [ ({U1 LHS1},{U1 RHS1}),({U1 LHS2},{U1 RHS2}) ],  [({U2 LHS},{U2 RHS})], ... ,[({Un LHS},{Un RHS})]}
+    return: list of lists of tuples of sets
+            list of Funtional Dependencies
+    i.e. [ [ ({U1 LHS1},{U1 RHS1}),({U1 LHS2},{U1 RHS2}) ],  [({U2 LHS},{U2 RHS})], ... ,[({Un LHS},{Un RHS})] ]
     '''
-    ret = []
+    partitions = []
     had = []
     for relation in minCover:
         LHS = relation[0]
         if LHS not in had:
-            ret.append([relation])
+            partitions.append([relation])
             had.append(LHS)
             continue
-        for l in ret:
+        for l in partitions:
             if (l[0][0] is LHS):
                 l.append(relation)
                 break;
         had.append(LHS)
-    return ret
+    return partitions
+
+def createSchemas(partitions):
+    '''
+    Creates a schema for each list of dependencies with equal LHS's
+
+    param:  list of lists of tuples of sets
+
+    return: dict
+        key = attributes in schema (set)
+        value = list of Functional Dependencies in schema (list of tuples of sets)
+    '''
+    schemas = []
+    for fdList in partitions:
+        attributes = set()
+        attributes |= fdList[0][0]
+        # add the rest of the attributes
+        for dep in fdList:
+            attributes |= dep[1]
+        schemas.append((attributes,fdList))
+        # schemas[attributes] = fdList
+    return schemas
--- test3nf.py
+++ test3nf.py
@@ -3,9 +3,10 @@ import sys
 import threenf
 import computations
 
-example = [({1, 2, 3}, {2, 3, 4, 5}), ({1}, {1}), ({}, {6}), ({3}, {})]
-expected = [({1, 2, 3}, {2}), ({1, 2, 3}, {3}), ({1, 2, 3}, {4}), ({1, 2, 3}, {5}), ({1}, {1}), ({}, {6})]
-expected2 = [[({1, 2, 3}, {2}), ({1, 2, 3}, {3}), ({1, 2, 3}, {4}), ({1, 2, 3}, {5})], [({1}, {1})], [({}, {6})]]
+example = [({1, 2, 3}, {2, 3, 4, 5}), ({1}, {1}), (set(), {6}), ({3}, set())]
+expected = [({1, 2, 3}, {2}), ({1, 2, 3}, {3}), ({1, 2, 3}, {4}), ({1, 2, 3}, {5}), ({1}, {1}), (set(), {6})]
+expected2 = [[({1, 2, 3}, {2}), ({1, 2, 3}, {3}), ({1, 2, 3}, {4}), ({1, 2, 3}, {5})], [({1}, {1})], [(set(), {6})]]
+expected3 = [({1,2,3,4,5},[({1, 2, 3}, {2}), ({1, 2, 3}, {3}), ({1, 2, 3}, {4}), ({1, 2, 3}, {5})]),({1},[({1}, {1})]),({6},[(set(), {6})])]
 
 test = threenf.break_up_RHS(example)
 print "Output (top) and expected output (bottom):"
@@ -21,4 +22,12 @@ print test
 print
 print expected2
 if test == expected2: print "Passed"
+print
+
+test = computations.createSchemas(test)
+print "Output (top) and expected output (bottom):"
+print test
+print
+print expected3
+if test == expected3: print "Passed"
 print
