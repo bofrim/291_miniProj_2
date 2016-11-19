9fd2dea72364068f13f03388c3ff48cfe4e17862
bofrim <bofrim@hotmail.com>
2016-11-19 13:05:40 -0700

    added function to break up RHS of FDs
    also changed the name of ‘compute_closure’ to ‘closure’

--- threenf.py
+++ threenf.py
@@ -1,2 +1,20 @@
 import sqlite3
 import sys
+
+def mininal_cover(attributes, FDs):
+    # 1. Make RHS of each FD into a single attribute
+    # 2. Eliminate redundant attributes from LHS.
+    # 3. Delete redundant FDs
+    #
+    # Assume FDs is a list of tupes of sets. The list is a list of
+    #   functional dependencies, the tupes represent each funcitional
+    #   dependency, the sets are the LHS and the RHS:
+    # [({LHS1}, {RHS1}), ({LHS2}, {RHS2}), ..., ({LHSn}, {RHSn})]
+    pass
+
+def break_up_RHS(FDs):
+    new_FDs = []
+    for FD in FDs:
+        for attribute in FD[1]:
+            new_FDs.append((FD[0], set([attribute])))
+    return new_FDs
